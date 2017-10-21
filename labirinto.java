17eff872a84316839ee6095507e01f6eee74c159
Bianca Dorta <30930818+biancadorta@users.noreply.github.com>
2017-10-21 16:14:30 -0200

    Update labirinto.java

--- labirinto.java
+++ labirinto.java
@@ -2,7 +2,7 @@ public class labirinto
 {
 	char[][] labirinto;
 
-    int TotalLinha;
+        int TotalLinha;
 	int TotalColuna;
 	char parede;
 	char espaco;
@@ -26,47 +26,47 @@ public labirinto(char[][] lab,int linha,int coluna){
 
 public boolean ConferirCaracteres() throws Exception{
 
-boolean AchouE = false;
-boolean AchouS = false;
-int Contador = 0;
-int aux = 2;
+	boolean AchouE = false;
+	boolean AchouS = false;
+	int Contador = 0;
+	int aux = 2;
 
-while(aux != 0){
+	while(aux != 0){
 
-	if(Contador == this.TotalLinha-1) //se for a segunda vez.... pois de uma linha com coluna só irei consultar a primeira linha e a ultima
-		aux = 0;
+		if(Contador == this.TotalLinha-1) //se for a segunda vez.... pois de uma linha com coluna só irei consultar a primeira linha e a ultima
+			aux = 0;
 
-	for(int coluna = 0; coluna <= this.TotalColuna-1;coluna++){
-		if((this.labirinto[Contador][coluna] == 'E')||(this.labirinto[Contador][coluna] == 'e')){
+		for(int coluna = 0; coluna <= this.TotalColuna-1;coluna++){
+			if((this.labirinto[Contador][coluna] == 'E')||(this.labirinto[Contador][coluna] == 'e')){
 
 			this.posicaoInicialLinha = Contador;
 			this.posicaoInicialColuna = coluna;
 			AchouE = true;
 		}
 
-	    if((this.labirinto[Contador][coluna] == 'S')||(this.labirinto[Contador][coluna] == 's')){
+	    	if((this.labirinto[Contador][coluna] == 'S')||(this.labirinto[Contador][coluna] == 's')){
 
 			this.posicaoFinalLinha = Contador;
 			this.posicaoFinalColuna = coluna;
 			AchouS = true;
-        }
+        	}
 
-    }
-       Contador = this.TotalLinha-1; //ele estará pegando a ultima linha que existe no labirinto -1 que é a ultima linha da minha matriz
-        //pois minha matriz inicia com 0
+   	 }
+      		 Contador = this.TotalLinha-1; 	  //ele estará pegando a ultima linha que existe no labirinto -1 que é a ultima linha da minha matriz
+      						  //pois minha matriz inicia com 0
  }
 
-	aux = 2;       //voltando o aux para 0
-	Contador = 0;  //voltando Contador para 0 para ser usado no próximo while... que será usado para conferir
-		           //as laterais da minha matriz.
-  while(aux != 0){
+		aux = 2;       //voltando o aux para 0
+		Contador = 0;  //voltando Contador para 0 para ser usado no próximo while... que será usado para conferir
+		               //as laterais da minha matriz.
+	  while(aux != 0){
 
-	if(Contador == this.TotalColuna-1) //se for a segunda vez.... pois  só irei consultar a primeira coluna e a ultima... que são
-			//minhas laterais onde posso encontrar E e S.
+		if(Contador == this.TotalColuna-1) //se for a segunda vez.... pois  só irei consultar a primeira coluna e a ultima... que são
+					           //minhas laterais onde posso encontrar E e S.
 		aux = 0;
 
-    for(int linha = 0; linha <= this.TotalLinha-1; linha++){
-		if((this.labirinto[linha][Contador] == 'E')||(this.labirinto[linha][Contador] == 'e')){
+    		for(int linha = 0; linha <= this.TotalLinha-1; linha++){
+			if((this.labirinto[linha][Contador] == 'E')||(this.labirinto[linha][Contador] == 'e')){
 
 			this.posicaoInicialLinha = linha;
 			this.posicaoInicialColuna = Contador;
@@ -81,20 +81,16 @@ while(aux != 0){
 	    }
 
        }
-      Contador = this.TotalColuna-1; //ele estará pegando a ultima Coluna que existe no labirinto -1 que é a ultima linha da minha matriz
-	        //pois minha matriz inicia com 0
+   		   Contador = this.TotalColuna-1; //ele estará pegando a ultima Coluna que existe no labirinto -1 que é a ultima linha da minha matriz
+	        				  //pois minha matriz inicia com 0
    }
-
     if((AchouS)&&(AchouE))
     	return true;
+	
     else{
 	    throw new Exception ("Não foi encontrado O caracter E ou S em seu Arquivo,porfavor certifique-se se seu arquivo está digitado corretamente");
-	    }
-
-
-
-
- }
+	}
+}
 
  public int PosicaoInicialLinha(){
  	return this.posicaoInicialLinha;
@@ -164,9 +160,13 @@ public boolean validaPosicao(int linha, int coluna)
        }
        if(this.labirinto[linha][coluna] == ' ')
        {
-	       
+		
+		posicaEValida = true;
+		System.out.println(linha+" , "+coluna+" : Ha espaco");
+	        //linhaValida3  = linha+1;
+ 	        //colunaValida3 = coluna;	       
        }
+	 return posicaoEValida;
+}
 
-	   return posicaoEValida;
-	}
 }
