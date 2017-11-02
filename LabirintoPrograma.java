c5d0366e613f0233b40a4557ad0d3250e243d163
Bianca Dorta <30930818+biancadorta@users.noreply.github.com>
2017-11-02 11:43:07 -0200

    Adicionando os arquivos das packages novamente
    Esta versão é a mais atualizada, contendo as packages organizadas corretamente e com a implementação do método de loop do labirinto

--- LabirintoPrograma.java
+++ LabirintoPrograma.java
@@ -1,172 +1,76 @@
-import java.io.*;
 
-public class LabirintoPrograma{
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import labirintoprograma.labirinto.labirinto;
 
-    //public static double[][] labirinto;
-	public static void main(String[] args)
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 17165, 17171 e 16
+ */
+public class LabirintoPrograma {
+ public static void main(String[] args) throws IOException
   {
+    try
+    {            
+        char[][] lab = null;
+        int totalLinha = 0;
+        int totalColuna = 0;         
+        labirinto l;         
+        String nomeArquivo = null;
+        
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Caminho para o arquivo?");
+	       	  String caminhoParaOArquivo=teclado.readLine();        
 
-    	char[][] labirinto;
-    	int TotalLinha = 0;  //elas estarão contando apartir de 1...(não estará na logica do vetor... onse a posicao inicial é 0 e nela é armazenada...)
-    	int TotalColuna= 0;  //elas estarão contando apartir de 1...(não estará na logica do vetor... onse a posicao inicial é 0 e nela é armazenada...)
+        BufferedReader arquivo = new BufferedReader(new FileReader(caminhoParaOArquivo));
 
-       // LENDO O ARQUIVO.
-      try
-      {
-
-
-
-             BufferedReader teclado = new BufferedReader(
-	                             		    new InputStreamReader(
-	                                   	    System.in));
-
-	          System.out.print ("Caminho para o arquivo? ");
-	       	  String caminhoParaOArquivo=teclado.readLine();
-
-	      	  BufferedReader arquivo = new BufferedReader(
-	                                	 new FileReader(
-	                                 		caminhoParaOArquivo));
-
-
-           //PEGANDO VALOR DA COLUNA E LINHA
-
-	           for(int linha = 1; linha <= 2; linha++)
-             {
-
-			   if(linha == 1)
-                 {
-		    	   	      String Arquivo = arquivo.readLine();
-		    	          TotalLinha = Integer.parseInt(Arquivo);
-
-		    		       System.out.println(TotalLinha);
-  				 }
-
-               if(linha == 2)
-                {
-		       	  	      String Arquivo = arquivo.readLine();
-		       	  	      TotalColuna = Integer.parseInt(Arquivo);
-
-	     	       	      System.out.println(TotalColuna);
-			     }
-
-            }
-        	    //Coord.setLinha(TotalLinha);
-        	    //Coord.setColuna(TotalColuna);
-
-        	   //caminho.empilhe(Coord);
-        	   //System.out.println("valor coord empilhe : "+caminho.toString());
-
-           	labirinto = new char[TotalLinha][TotalColuna];  //matriz de char
-
-
-          //Colocar informações na matriz e printar...
-          	int contagemLinha = 0;
-
-          	 while (arquivo.ready())
-          	 {
-
-	      	      String componente = arquivo.readLine(); //armazenando em uma var os valores da linha em questao do arquivo
-
-	      	      for(int coluna = 0; coluna <= TotalColuna-1; coluna++) // ja percoreu a linha 0 e colunas
-	      	         labirinto[contagemLinha][coluna] = componente.charAt(coluna); //pegando cada char da linha para colocar na posicao da matriz adequada.
-          																	      //ele começara com a ContagemLinha qúe é o int que declarei acima, ela funciona para saber de qual linha estou tratando...
-          																	     //para poder aramazenar os dados corretamente conforme o arquivo txt...
-
-          	      for(int coluna = 0; coluna <= TotalColuna-1; coluna++)
-            	  	   System.out.print(labirinto[contagemLinha][coluna] );
-                       System.out.println("");
-                       contagemLinha++; //agora estou indo para próxima linha pois todos os character da linha já foram pegos.
-
-		      }
-
-
-
-         labirinto<Coordenada> proc = new labirinto<Coordenada>(labirinto,TotalLinha,TotalColuna);
-
-        // System.out.println("VALORES : " + caminho.toString());
-
-        if(proc.ConferirCaracteres())
-        {
-			int qtdPosicoes = TotalLinha * TotalColuna;
-			Pilha<Coordenada> caminho = new Pilha<>(qtdPosicoes,qtdPosicoes);  // minha taixa vai ser igual a quantidade.
-			Pilha<Fila<Coordenada>> possibilidades = new Pilha<>(qtdPosicoes); //possibilidades armazenara no maximo 40 filas de coordenadas
-
-
-			//Coordenada Coord = new Coordenada();
-			//Coord.setLinha(TotalLinha);
-			//Coord.setColuna(TotalColuna);
-         	//caminho.empilhe(Coord); //Ele empilhara somente a posicao atual
-
-
-			Coordenada atual      = new Coordenada(proc.colunaInicial(),proc.linhaInicial()); //armazenar nessa variavel onde encontrou-se E(inicialmente)
-
-			//atual.setLinha(proc.linhaInicial());
-			//atual.setColuna(proc.colunaInicial());
-
-			System.out.println(atual.toString());
-
-			Fila<Coordenada> fila = new Fila<Coordenada>(3); //esta enfileira as posicoes adjacentes a atual, direita, em cima, ou em baixo
-			proc.validaPosicao(atual.getLinha(),atual.getColuna());
-			//labirinto<Coordenada> teste = new labirinto<>(labirinto,TotalLinha,TotalColuna);
-
-			if(proc.posicaoValida(proc.getLinhaValida1(),proc.getColunaValida1()))
-			{
-				Coordenada unicornio  = new Coordenada(proc.getColunaValida1(),proc.getLinhaValida1());
-				fila.enfileire(unicornio);
-				System.out.println("{"+proc.getLinhaValida1()+ ", "+proc.getColunaValida1()+"}"+" é valida");
-			}
-
-			if(proc.posicaoValida(proc.getLinhaValida2(),proc.getColunaValida2()))
-			{
-				Coordenada unicornio2 = new Coordenada(proc.getColunaValida2(),proc.getLinhaValida2());
-				fila.enfileire(unicornio2);
-				System.out.println("{"+proc.getLinhaValida2()+", "+proc.getColunaValida2()+ "}"+" é valida");
-			}
-			if(proc.posicaoValida(proc.getLinhaValida3(),proc.getColunaValida3()))
-			{
-				Coordenada unicornio3 = new Coordenada(proc.getColunaValida3(),proc.getLinhaValida3());
-				fila.enfileire(unicornio3);
-				System.out.println("Colun3 é valida");
-			}
-			System.out.println(fila.toString());
-
-			//fila.enfileire(); //enfileirar a posicoes validas
-			proc.colocarPasso(atual.getLinha(),atual.getColuna());
-			//atual.setColuna(2);
-		    //proc.colocarPasso(atual.getLinha(),atual.getColuna());
-			//proc.colocarPasso(atual.getLinha(),atual.getColuna());
-			//atual.setLinha(3);
-		    //proc.colocarPasso(atual.getLinha(),atual.getColuna());
-			atual = fila.getFila();
-			fila.desenfileire();
-			//fila.enfileire(proc.coordValidas());
-			caminho.empilhe(atual);
-
-			//atual.setColuna(2);
-			System.out.println(caminho.toString());
-
+        for(int linha = 1; linha <= 2; linha++){
+        if(linha == 1){
+            String Arquivo = arquivo.readLine();
+            totalLinha = Integer.parseInt(Arquivo);
+            System.out.println(totalLinha);
+                    }
+        
+        if(linha == 2){
+        String Arquivo = arquivo.readLine();
+        totalColuna = Integer.parseInt(Arquivo);
+        System.out.println(totalColuna);
+                }
         }
-	 }
+        char[][] labirinto = new char[totalLinha][totalColuna];
+        int contagemLinha = 0;       
 
-	  catch (FileNotFoundException e1)
-      {
-	    	  	System.out.println("Nao foi possivel encontrar o caminho especificado, por favor digite o nome correto,ou certifique-se se o arquivo existe");
-      }
+        while (arquivo.ready())
+        {
+            String componente = arquivo.readLine(); //armazenando em uma var os valores da linha em questao do arquivo
 
-      catch(ArrayIndexOutOfBoundsException e2)
-      {
-			   System.out.println("Não foi possível realizar o procedimento de leitura com sucesso.");
-			   System.out.println("Confira seu Arquivo texto,ele apressenta número de linhas ou colunas errado :(");
-	  }
+            for(int coluna = 0; coluna <= totalColuna-1; coluna++){ // ja percoreu a linha 0 e colunas
+                labirinto[contagemLinha][coluna] = componente.charAt(coluna);
+                System.out.print(labirinto[contagemLinha][coluna]);
+            }
+            System.out.println("");
+            contagemLinha++;                                                                                                                                                  //ele comeÃ§ara com a ContagemLinha qÃºe Ã© o int que declarei acima, ela funciona para saber de qual linha estou tratando...
+          
+      }         
+         l = new labirinto(labirinto,totalLinha,totalColuna); 
+         l.resolverLab();
+       
+    }   
+    catch(FileNotFoundException erro2)
+    { System.out.println("NÃ£o foi achado o arquivo.");}
 
-	  catch(Exception e3)
-      {
-			   System.out.println("Erro :"+ e3);
-	  }
+    catch(Exception erro)
+    { System.out.println("Labirinto errado");}
 
-      /* Acabamos de ler o arquivo e colocar cada posição na matriz...
-       Agora iremos estanciar os objetos da classe caso possuam E e S indicando o inicio do labirinto e o fim do labirinto.
-       Se não ira abortar e ira apontar o erro...
-      */
-    }
+  }   
+    
 }
